2a3d67cc3f8b5ffcaacd4c67c37d695446498c8c
solflower <60803614+solflower@users.noreply.github.com>
2023-07-25 18:10:16 +0700

    add element in box

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 
 export default function Counter() {
     const [num, setNum] = useState(0)
+    const [things, setThings] = useState(["thing1", "thing2"])
 
     function add(){
         setNum(prevNum => prevNum+1)
@@ -11,11 +12,23 @@ export default function Counter() {
         setNum(prevNum => prevNum-1)
     }
 
+    function addThing(){
+        setThings(prevThings => {
+            return [...prevThings, `Thing${prevThings.length+1}`]
+        })
+    }
+
     return (
-        <div className='section'>
-            <button onClick={sub} className='btn'>-</button>
-            <p className='paragraph'>{num}</p>
-            <button onClick={add} className='btn'>+</button>
+        <div>
+            <div className='section'>
+                <button onClick={sub} className='btn'>-</button>
+                <p className='paragraph'>{num}</p>
+                <button onClick={add} className='btn'>+</button>
+            </div>
+            <div className='section'>
+                <div className = "cardv2">{things}</div>
+                <button onClick={addThing} className='btn'>add</button>
+            </div>
         </div>
     )
 }
